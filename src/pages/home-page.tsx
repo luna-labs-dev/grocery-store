c393d01e5a2eda8015c90b30c303d26bd1e0a800
Tiago Poli <tiagoluizpoli@gmail.com>
2025-05-03 02:08:32 -0300

    feat: add loading animation for isSignedIn verification

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,8 @@
 export const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full h-full">
       <h1>Home Page</h1>
+      {/* <LoadingCart className="w-[300px] h-[150px]" size={60} /> */}
     </div>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
-import { useFirebase } from '@/providers';
+import { useAuth } from '@clerk/clerk-react';
 import { protectedRoutes } from './protected-routes';
 import { publicRoutes } from './public-routes';
 
 export const useRoutes = () => {
-  const { context } = useFirebase();
+  const { isLoaded, isSignedIn } = useAuth();
+  console.log({ isLoaded, isSignedIn });
 
-  const routes = context.userLoggedIn ? protectedRoutes : publicRoutes;
+  const routes = isSignedIn ? protectedRoutes : publicRoutes;
   // const routes = publicRoutes;
 
-  return { routes };
+  return { routes, isLoaded };
 };
