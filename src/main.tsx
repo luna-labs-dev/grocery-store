3f57a4d8476f407cfe76fa9c4777b8ffed853981
Tiago Poli <tiagoluizpoli@gmail.com>
2026-01-26 03:58:28 -0300

    add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import './index.css';
-import { App } from './app.tsx';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen.ts';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>,
   );
 }
