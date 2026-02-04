d5e34623dbef49b652e4994bf6c9244fa6fde31f
Tiago Poli <tiagoluizpoli@gmail.com>
2026-02-04 07:40:30 -0300

    loading state wip

--- src/features/shopping-event/features/shopping-event-list/components/shopping-event-list.tsx
+++ src/features/shopping-event/features/shopping-event-list/components/shopping-event-list.tsx
@@ -1,51 +1,29 @@
-import { useNavigate } from '@tanstack/react-router';
-import { useState } from 'react';
 import { ShoppingEventItem } from './shopping-event-item';
-import { Button, CustomPagination } from '@/components';
 import type { FetchShoppingEventListParams } from '@/features/shopping-event/domain';
 import { useGetShoppingEventListQuery } from '@/features/shopping-event/infrastructure';
 
-export const ShoppingEventList = () => {
-  const navigate = useNavigate();
-  const [paginationParams, setPaginationParams] =
-    useState<FetchShoppingEventListParams>({
-      pageIndex: 0,
-      pageSize: 10,
-      orderBy: 'createdAt',
-      orderDirection: 'desc',
-    });
+interface Props {
+  paginationParams: FetchShoppingEventListParams;
+}
 
-  const { data, isError } = useGetShoppingEventListQuery(paginationParams);
+export const ShoppingEventList = ({ paginationParams }: Props) => {
+  const { data, isFetching, isError } =
+    useGetShoppingEventListQuery(paginationParams);
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
 
   return (
     <div className="flex flex-col gap-4 mb-4">
-      <div className="flex flex-col md:flex-row gap-4 md:gap-0 items-center justify-between">
-        <Button
-          onClick={() =>
-            navigate({
-              to: '/shopping-event/start-shopping-event',
-            })
-          }
-          className="w-full md:w-fit"
-        >
-          Novo Evento
-        </Button>
-        <CustomPagination
-          paginationProps={{
-            paginationParams,
-            setPaginationParams,
-            listTotal: data?.total,
-          }}
-        />
-      </div>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {isError ? (
-          <div>Error loading data</div>
-        ) : (
-          data?.items.map((se) => (
-            <ShoppingEventItem key={se.id} shoppingEvent={se} />
-          ))
-        )}
+        {data?.items.map((se) => (
+          <ShoppingEventItem key={se.id} shoppingEvent={se} />
+        ))}
       </div>
     </div>
   );
--- src/features/shopping-event/features/shopping-event-list/shopping-event-page.tsx
+++ src/features/shopping-event/features/shopping-event-list/shopping-event-page.tsx
@@ -1,9 +1,42 @@
+import { useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
+import { useGetShoppingEventListQuery } from '../../infrastructure';
 import { ShoppingEventList } from './components/shopping-event-list';
-
+import { Button, CustomPagination } from '@/components';
+import type { FetchShoppingEventListParams } from '@/features/shopping-event/domain';
 export const ShoppingEventPage = () => {
+  const navigate = useNavigate();
+  const [paginationParams, setPaginationParams] =
+    useState<FetchShoppingEventListParams>({
+      pageIndex: 0,
+      pageSize: 10,
+      orderBy: 'createdAt',
+      orderDirection: 'desc',
+    });
+
+  const { data } = useGetShoppingEventListQuery(paginationParams);
   return (
-    <div className="flex flex-col gap-4">
-      <ShoppingEventList />
+    <div>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-0 items-center justify-between">
+        <Button
+          onClick={() =>
+            navigate({
+              to: '/shopping-event/start-shopping-event',
+            })
+          }
+          className="w-full md:w-fit"
+        >
+          Novo Evento
+        </Button>
+        <CustomPagination
+          paginationProps={{
+            paginationParams,
+            setPaginationParams,
+            listTotal: data?.total,
+          }}
+        />
+      </div>
+      <ShoppingEventList paginationParams={paginationParams} />
     </div>
   );
 };
