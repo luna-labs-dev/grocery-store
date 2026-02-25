d0515887ce0bf638d71983752286e2db6e464435
Tiago Poli <tiagoluizpoli@gmail.com>
2026-02-25 02:20:45 -0300

    chore: fix script

--- apps/backend/scripts/server.sh
+++ apps/backend/scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(cd /app/apps/backend && node dist/infrastructure/repositories/drizzle/setup/migrate.js)
-(cd /app/apps/backend && node dist/main/server.js)
+(cd /app/apps/backend && node dist/src/infrastructure/repositories/drizzle/setup/migrate.js)
+(cd /app/apps/backend && node dist/src/main/server.js)
