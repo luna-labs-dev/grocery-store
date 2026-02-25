67f1554abcebb5e27f03559e4210980a9bb9be47
Tiago Poli <tiagoluizpoli@gmail.com>
2026-02-25 01:00:59 -0300

    chore: adjust ci

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,15 +1,22 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-import { env } from './src/main/config/env';
 
 config();
-const { database } = env;
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} = process.env;
+
+const url = `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
 export default defineConfig({
   schema: './src/infrastructure/repositories/drizzle/setup/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: database.url,
+    url,
   },
   verbose: true,
   strict: true,
--- apps/backend/scripts/server.sh
+++ apps/backend/scripts/server.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-(echo 'root')
-(ls)
-(cd /app/apps/backend && node_modules/.bin/prisma generate)
-(cd /app/apps/backend && node_modules/.bin/prisma migrate deploy)
-(cd /app/apps/backend && node dist/prisma/seed.js)
-(cd /app/apps/backend && node dist/src/main/server.js)
+
+(cd /app/apps/backend && node dist/infrastructure/repositories/drizzle/setup/migrate.js)
+(cd /app/apps/backend && node dist/main/server.js)
